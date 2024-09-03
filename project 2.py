f6edfa92861f361b668fadcc1b3de3e50722788c
Saikrish445 <chsaikrishna9640@gmail.com>
2024-09-03 22:39:58 +0530

    Create project 2.py

---
+++ project 2.py
@@ -0,0 +1,15 @@
+import random
+
+random_number = random.randint(1, 100)
+attempts = 0
+
+while True:
+    user_input = int(input("Guess the number: "))
+    attempts += 1
+    if user_input < random_number:
+        print("The number you entered is too low.")
+    elif user_input > random_number:
+        print("The number you entered is too high.")
+    else:
+        print(f"You guessed the number in {attempts} attempts!")
+        break
